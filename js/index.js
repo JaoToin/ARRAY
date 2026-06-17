31cfe4bd8ff6a73a3d6ae4c6fd72fab7753c9e5a
joao <joao.a.guedes@aluno.senai.br>
2026-06-17 20:57:05 -0300

    ARRAY

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//DECLARAR ARRAY
+
+//               0   1   2    3    4
+const numeros = [10, 50, 60, 300, 70]
+
+console.log(numeros[3])
+
+numeros[2] = 124
+
+console.log(numeros[0])
+console.log(numeros[1])
+console.log(numeros[2])
+console.log(numeros[3])
+console.log(numeros[4])
